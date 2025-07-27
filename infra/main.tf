4989ecaea76b792e889720bb461e4e731b2453f9
Whittaker555 <whittaker555@hotmail.co.uk>
2025-07-27 19:59:47 +0100

    Update lambda runtime to provided.al2

--- infra/main.tf
+++ infra/main.tf
@@ -27,8 +27,8 @@ resource "aws_iam_role_policy_attachment" "lambda_basic" {
 resource "aws_lambda_function" "api" {
   function_name = var.app_name
   role          = aws_iam_role.lambda.arn
-  runtime       = "go1.x"
-  handler       = "main"
+  runtime       = "provided.al2"
+  handler       = "bootstrap"
 
   filename         = "${path.module}/../build/${var.app_name}.zip"
   source_code_hash = filebase64sha256("${path.module}/../build/${var.app_name}.zip")
@@ -46,9 +46,9 @@ resource "aws_apigatewayv2_api" "http" {
 }
 
 resource "aws_apigatewayv2_integration" "lambda" {
-  api_id             = aws_apigatewayv2_api.http.id
-  integration_type   = "AWS_PROXY"
-  integration_uri    = aws_lambda_function.api.invoke_arn
+  api_id                 = aws_apigatewayv2_api.http.id
+  integration_type       = "AWS_PROXY"
+  integration_uri        = aws_lambda_function.api.invoke_arn
   payload_format_version = "2.0"
 }
 
