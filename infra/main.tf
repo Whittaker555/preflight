8f1573454142f3dc8d98507c2dc2c9d547c5a3e4
Whittaker555 <whittaker555@hotmail.co.uk>
2025-07-27 18:09:28 +0100

    Switch to AWS Lambda deployment

---
+++ infra/main.tf
@@ -0,0 +1,77 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+data "aws_caller_identity" "current" {}
+
+resource "aws_iam_role" "lambda" {
+  name               = "${var.app_name}-lambda"
+  assume_role_policy = data.aws_iam_policy_document.lambda_assume.json
+}
+
+data "aws_iam_policy_document" "lambda_assume" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_basic" {
+  role       = aws_iam_role.lambda.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_lambda_function" "api" {
+  function_name = var.app_name
+  role          = aws_iam_role.lambda.arn
+  runtime       = "go1.x"
+  handler       = "main"
+
+  filename         = "${path.module}/../build/${var.app_name}.zip"
+  source_code_hash = filebase64sha256("${path.module}/../build/${var.app_name}.zip")
+
+  environment {
+    variables = {
+      PORT = "3000"
+    }
+  }
+}
+
+resource "aws_apigatewayv2_api" "http" {
+  name          = "${var.app_name}-api"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_integration" "lambda" {
+  api_id             = aws_apigatewayv2_api.http.id
+  integration_type   = "AWS_PROXY"
+  integration_uri    = aws_lambda_function.api.invoke_arn
+  payload_format_version = "2.0"
+}
+
+resource "aws_apigatewayv2_route" "default" {
+  api_id    = aws_apigatewayv2_api.http.id
+  route_key = "$default"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda.id}"
+}
+
+resource "aws_apigatewayv2_stage" "default" {
+  api_id      = aws_apigatewayv2_api.http.id
+  name        = "$default"
+  auto_deploy = true
+}
+
+resource "aws_lambda_permission" "apigw" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.api.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.http.execution_arn}/*/*"
+}
+
+output "api_url" {
+  value = aws_apigatewayv2_stage.default.invoke_url
+}
