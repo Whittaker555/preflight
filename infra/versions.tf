4eb9c0b294030230fc96286e127dac039c20ca1e
Whittaker555 <Whittaker555@hotmail.co.uk>
2025-07-28 21:06:10 +0100

    backend settings for terrraform stte

--- infra/versions.tf
+++ infra/versions.tf
@@ -1,4 +1,9 @@
 terraform {
+  backend "s3" {
+    bucket = "preflight-state-bucket"
+    key = "preflight-api/terraform.tfstate"
+    region = "eu-west-2"
+  }
   required_version = ">= 1.5"
   required_providers {
     aws = {
